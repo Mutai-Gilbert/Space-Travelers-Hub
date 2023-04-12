81abff484791c8ad5201b3a85fff800519b1e88c
Ernest Korir <ernestbore3@gmail.com>
2023-04-12 10:45:42 +0300

    Update SingleRocket

---
+++ src/components/SingleRocket.jsx
@@ -0,0 +1,29 @@
+import { useDispatch } from 'react-redux';
+import React from 'react';
+import { toggleRocket } from '../redux/rocketsSlice';
+
+const Rocket = ({
+  id, rocketName: name, description, flickrImages, reserved,
+}) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="container">
+      <div className="image">{flickrImages}</div>
+      <div className="rocketDetails">
+        <h1>{name}</h1>
+        <p className="description">
+          <span className="reservation">
+            {' '}
+            {reserved ? 'Reserved' : ' '}
+            {' '}
+          </span>
+          {description}
+        </p>
+        <button type="button" onClick={() => dispatch(toggleRocket(id))}>Reserve Rocket</button>
+      </div>
+    </div>
+  );
+};
+
+export default Rocket;
