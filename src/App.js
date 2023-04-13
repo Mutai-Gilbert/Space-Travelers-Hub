b72e2fef953542d6e222eede7f5ea2e3780c1d2a
mutai-gilbert <mutaigilbert85@gmail.com>
2023-04-13 09:55:14 +0300

    Added new files to the Profile.js and created Profile.module.css file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import './App.css';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { getMissions } from './redux/mission/missionSlice';
 import ErrorPage from './pages/ErrorPage';
 import MissionsPage from './pages/MissionsPage';
 import Profile from './pages/Profile';
 import Rockets from './pages/Rockets';
 import SharedLayout from './pages/SharedLayout';
 
-const App = () => (
-  <Routes>
-    <Route path="/" element={<SharedLayout />}>
-      <Route index element={<Rockets />} />
-      <Route path="/missions" element={<MissionsPage />} />
-      <Route path="profile" element={<Profile />} />
-    </Route>
-    <Route path="*" element={<ErrorPage />} />
-  </Routes>
-);
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMissions());
+  }, [dispatch]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Rockets />} />
+        <Route path="/missions" element={<MissionsPage />} />
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  );
+};
 
 export default App;
